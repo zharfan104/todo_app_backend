3952462ad942626d8d21643bb7cec33e713cd10b
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-04 19:16:58 +0700

    Add _verifyTokenHandler on middleware

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,14 +1,38 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:in_memory_todos_data_source/in_memory_todos_data_source.dart';
+import 'package:todo_app_backend/core/helpers/response_helper.dart';
 import 'package:todo_app_backend/core/jwt/jwt_service.dart';
+import 'package:todo_app_backend/di.dart';
 import 'package:todos_data_source/todos_data_source.dart';
 
 final _dataSource = InMemoryTodosDataSource();
-final JwtService jwtService = JwtServiceImpl();
+const _authenticationUrl = ['login', 'register'];
 
 Handler middleware(Handler handler) {
   return handler
-      // .use(requestLogger())
-      .use(provider<TodosDataSource>((_) => _dataSource))
-      .use(provider<JwtService>((_) => jwtService));
+      .use(requestLogger())
+      .use(
+        provider<TodosDataSource>((_) => _dataSource),
+      )
+      .use(_verifyTokenHandler);
+}
+
+Handler _verifyTokenHandler(Handler handler) {
+  return (context) async {
+    final middleWare = sl<JwtService>();
+    final isTokenVerified = await middleWare.verifyToken(context);
+    final url = context.request.url.toString();
+    final isAuthenticationUrl = _authenticationUrl.contains(url);
+    final isNeedVerifiedToken = !isAuthenticationUrl;
+
+    if (isNeedVerifiedToken) {
+      if (!isTokenVerified) {
+        return ResponseHelper.unAuthorized();
+      }
+    }
+
+    final response = await handler(context);
+
+    return response;
+  };
 }
