49ec8b3546f5063c0957a47621699f00c1aed8de
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-04 15:58:28 +0700

    Add JwtService to middleware

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,13 +1,14 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:in_memory_todos_data_source/in_memory_todos_data_source.dart';
+import 'package:todo_app_backend/core/jwt/jwt_service.dart';
 import 'package:todos_data_source/todos_data_source.dart';
 
 final _dataSource = InMemoryTodosDataSource();
+final JwtService jwtService = JwtServiceImpl();
 
 Handler middleware(Handler handler) {
-  return handler.use(requestLogger()).use(
-        provider<TodosDataSource>(
-          (_) => _dataSource,
-        ),
-      );
+  return handler
+      // .use(requestLogger())
+      .use(provider<TodosDataSource>((_) => _dataSource))
+      .use(provider<JwtService>((_) => jwtService));
 }
