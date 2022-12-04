a83ca812ce6f17dba2a1cc46ad241f99b612c73a
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-04 10:48:45 +0700

    Update routes index.dart

--- test/routes/index_test.dart
+++ test/routes/index_test.dart
@@ -4,7 +4,7 @@ import 'package:dart_frog/dart_frog.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-import '../../routes/index.dart' as route;
+import '../../routes/todos/index.dart' as route;
 
 class _MockRequestContext extends Mock implements RequestContext {}
 
