f0266a3e80591385699937ed1ed337cb2d194647
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-04 16:40:39 +0700

    Add TaskModel and TaskEntity

---
+++ lib/features/tasks/data/models/task_model.dart
@@ -0,0 +1,25 @@
+import 'package:todo_app_backend/features/tasks/domain/entity/task_entity.dart';
+
+class TaskModel extends TaskEntity {
+  const TaskModel({
+    required super.id,
+    required super.completed,
+    required super.description,
+  });
+
+  factory TaskModel.fromJson(Map<String, dynamic> json) {
+    return TaskModel(
+      id: json['id'] as int,
+      completed: json['completed'] as bool,
+      description: json['description'].toString(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'completed': completed,
+      'description': description,
+    };
+  }
+}
---
+++ lib/features/tasks/domain/entity/task_entity.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+
+class TaskEntity extends Equatable {
+  const TaskEntity({
+    required this.id,
+    required this.completed,
+    required this.description,
+  });
+
+  final int id;
+  final bool completed;
+  final String description;
+
+  @override
+  List<Object?> get props => [id, completed, description];
+}
