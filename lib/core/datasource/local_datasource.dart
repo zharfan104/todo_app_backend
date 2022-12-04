c82d73869ad1e5ed6df6b254c3e3c9ec8a5597f4
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-04 18:42:14 +0700

    Add calling methods in LocalDataSource

--- lib/core/datasource/local_datasource.dart
+++ lib/core/datasource/local_datasource.dart
@@ -2,6 +2,13 @@ import 'package:todo_app_backend/features/tasks/data/models/task_model.dart';
 import 'package:todo_app_backend/features/users/data/models/user_model.dart';
 
 abstract class LocalDataSource {
+  Future<void> addUser(UserModel user);
+  Future<void> updateUser(UserModel user);
+  Future<UserModel> getUser(int userId);
   Future<List<UserModel>> getAllUsers();
+
+  Future<void> addTask(int userId, TaskModel task);
+  Future<void> deleteTask(int taskId);
+  Future<void> updateTask(TaskModel task);
   Future<List<TaskModel>> getAllTasks(int userId);
 }
--- lib/core/datasource/local_datasource_impl.dart
+++ lib/core/datasource/local_datasource_impl.dart
@@ -5,11 +5,55 @@ import 'package:todo_app_backend/features/tasks/data/models/task_model.dart';
 import 'package:todo_app_backend/features/users/data/models/user_model.dart';
 
 class LocalDataSourceImpl extends LocalDataSource {
+  final database = sl<PostgreSQLConnection>();
+
+  @override
+  Future<void> addUser(UserModel user) async {
+    await database.mappedResultsQuery(
+      '''
+      INSERT INTO public."User"(name, user_name, password, age)
+      VALUES ('${user.name}', '${user.userName}', '${user.password}', ${user.age});
+      ''',
+    );
+  }
+
+  @override
+  Future<void> updateUser(UserModel user) async {
+    await database.mappedResultsQuery(
+      '''
+      UPDATE Public."User"
+      SET name = '${user.name}',
+      age = ${user.age}, 
+      user_name = '${user.userName}', 
+      password = '${user.password}',
+      WHERE id = ${user.id};
+      ''',
+    );
+  }
+
+  @override
+  Future<UserModel> getUser(int userId) async {
+    final rawUsers = await database.mappedResultsQuery(
+      '''
+      select * from Public."User"
+      where id = $userId
+      ''',
+    );
+
+    final userModels = List<UserModel>.from(
+      rawUsers.map(
+        (user) => UserModel.fromJson(user['User']!),
+      ),
+    );
+
+    return userModels.first;
+  }
+
   @override
   Future<List<UserModel>> getAllUsers() async {
-    final database = sl<PostgreSQLConnection>();
-
-    final rawUsers = await database.mappedResultsQuery('select * from Public."User"');
+    final rawUsers = await database.mappedResultsQuery(
+      '''select * from Public."User"''',
+    );
 
     final userModels = List<UserModel>.from(
       rawUsers.map(
@@ -21,9 +65,38 @@ class LocalDataSourceImpl extends LocalDataSource {
   }
 
   @override
-  Future<List<TaskModel>> getAllTasks(int userId) async {
-    final database = sl<PostgreSQLConnection>();
+  Future<void> addTask(int userId, TaskModel task) async {
+    await database.mappedResultsQuery(
+      '''
+      INSERT INTO public."Task"(description, completed, user_id)
+      VALUES ('${task.description}', ${task.completed}, $userId);
+      ''',
+    );
+  }
 
+  @override
+  Future<void> deleteTask(int taskId) async {
+    await database.mappedResultsQuery(
+      '''
+      DELETE FROM public."Task"
+      Where id = $taskId;
+      ''',
+    );
+  }
+
+  @override
+  Future<void> updateTask(TaskModel task) async {
+    await database.mappedResultsQuery(
+      '''
+      UPDATE Public."Task"
+      SET description = '${task.description}', completed = ${task.completed}, 
+      WHERE id = ${task.id};
+      ''',
+    );
+  }
+
+  @override
+  Future<List<TaskModel>> getAllTasks(int userId) async {
     final rawTasks = await database.mappedResultsQuery(
       '''
       select * from Public."Task"
