8b5275277b8d33dd61aac3e0fbb9c35d473bcf0c
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-04 16:00:44 +0700

    Add UserRepositoryImpl

---
+++ lib/features/users/data/repositories/user_repository_impl.dart
@@ -0,0 +1,14 @@
+import 'package:todo_app_backend/features/users/data/datasource/local_datasource.dart';
+import 'package:todo_app_backend/features/users/data/datasource/local_datasource_impl.dart';
+import 'package:todo_app_backend/features/users/data/models/user_model.dart';
+import 'package:todo_app_backend/features/users/domain/repositories/user_repository.dart';
+
+class UserRepositoryImpl extends UserRepository {
+  final LocalDataSource localDataSource = LocalDataSourceImpl();
+
+  @override
+  Future<List<UserModel>> getAllUsers() async {
+    final users = await localDataSource.getAllUsers();
+    return users;
+  }
+}
