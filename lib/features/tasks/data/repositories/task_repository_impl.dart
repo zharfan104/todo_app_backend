aace912abb676d49fccb67538c13a3b4995981cc
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-04 16:41:21 +0700

    Add TaskRepositoryImpl

---
+++ lib/features/tasks/data/repositories/task_repository_impl.dart
@@ -0,0 +1,15 @@
+import 'package:todo_app_backend/core/datasource/local_datasource.dart';
+import 'package:todo_app_backend/core/datasource/local_datasource_impl.dart';
+import 'package:todo_app_backend/features/tasks/data/models/task_model.dart';
+import 'package:todo_app_backend/features/tasks/domain/repositories/task_repository.dart';
+
+class TaskRepositoryImpl extends TaskRepository {
+  final LocalDataSource localDataSource = LocalDataSourceImpl();
+
+  @override
+  Future<List<TaskModel>> getAllTasks(int userId) async {
+    final tasks = await localDataSource.getAllTasks(userId);
+
+    return tasks;
+  }
+}
