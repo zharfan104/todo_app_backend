bf91130eb6628b853db12480ebd7fc8b534c4557
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-05 21:15:11 +0700

    Add getTask on TaskRepository, and change localDataSource initialization to be from argument on TaskRepositoryImpl

--- lib/features/tasks/data/repositories/task_repository_impl.dart
+++ lib/features/tasks/data/repositories/task_repository_impl.dart
@@ -1,12 +1,13 @@
 import 'package:injectable/injectable.dart';
 import 'package:todo_app_backend/core/datasource/datasource.dart';
-import 'package:todo_app_backend/di.dart';
 import 'package:todo_app_backend/features/tasks/data/models/task_model.dart';
 import 'package:todo_app_backend/features/tasks/domain/repositories/task_repository.dart';
 
 @Singleton(as: TaskRepository)
 class TaskRepositoryImpl extends TaskRepository {
-  final DataSource localDataSource = sl();
+  TaskRepositoryImpl({required this.localDataSource});
+
+  final DataSource localDataSource;
 
   @override
   Future<void> addTask(int userId, TaskModel task) => localDataSource.addTask(userId, task);
@@ -22,5 +23,12 @@ class TaskRepositoryImpl extends TaskRepository {
     final tasks = await localDataSource.getAllTasks(userId);
 
     return tasks;
+  }
+
+  @override
+  Future<TaskModel> getTask(int taskId) async {
+    final task = await localDataSource.getTask(taskId);
+
+    return task;
   }
 }
--- lib/features/tasks/domain/repositories/task_repository.dart
+++ lib/features/tasks/domain/repositories/task_repository.dart
@@ -4,5 +4,6 @@ abstract class TaskRepository {
   Future<void> addTask(int userId, TaskModel task);
   Future<void> deleteTask(int taskId);
   Future<void> updateTask(TaskModel task);
+  Future<TaskModel> getTask(int taskId);
   Future<List<TaskModel>> getAllTasks(int userId);
 }
