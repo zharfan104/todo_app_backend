07559a1ed763ddac2167bb2cfd215d0c9b7b3f00
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-11 19:12:07 +0700

    Update LoginRequest

--- lib/features/login/presentations/login_request.dart
+++ lib/features/login/presentations/login_request.dart
@@ -11,36 +11,43 @@ import 'package:todo_app_backend/features/users/domain/usecases/get_all_users.da
 class LoginRequest {
   const LoginRequest._();
 
-  static Future<Response> loginUser(RequestContext context) async {
+  static Future<Response> login(RequestContext context) async {
+    if (ContentTypeHelper.identify(context) == ContentType.json) {
+      final requestBody = await context.shouldBindJSON();
+
+      return loginUser(
+        email: requestBody['email'] as String?,
+        password: requestBody['password'] as String?,
+      );
+    } else {
+      return ResponseHelper.badRequest(message: 'Request is not a JSON');
+    }
+  }
+
+  static Future<Response> loginUser({required String? email, required String? password}) async {
     final middleWare = sl<JwtService>();
     final randomNumber = math.Random().nextInt(999);
     final getAllUsersUsescase = GetAllUsers();
     final users = await getAllUsersUsescase();
 
-    if (ContentTypeHelper.identify(context) == ContentType.json) {
-      final requestBody = await context.shouldBindJSON();
+    final loggedinUser = users.where(
+      (element) => element.email == email && element.password == password,
+    );
 
-      final loggedinUser = users.where(
-        (element) => element.userName == requestBody['user_name'] && element.password == requestBody['password'],
-      );
+    final token = await middleWare.generateToken({
+      'id': loggedinUser.isEmpty ? randomNumber : loggedinUser.first.id,
+    });
 
-      final token = await middleWare.generateToken({
-        'id': loggedinUser.isEmpty ? randomNumber : loggedinUser.first.id,
-      });
-
-      return loggedinUser.isEmpty
-          ? ResponseHelper.abortWithStatus(
-              status: HttpStatus.ok,
-              message: 'User not found',
-            )
-          : ResponseHelper.json(
-              status: HttpStatus.ok,
-              message: 'Login Successfully',
-              token: token,
-              body: loggedinUser.first,
-            );
-    } else {
-      return ResponseHelper.badRequest(message: 'Request is not a JSON');
-    }
+    return loggedinUser.isEmpty
+        ? ResponseHelper.abortWithStatus(
+            status: HttpStatus.badRequest,
+            message: 'User not found',
+          )
+        : ResponseHelper.json(
+            status: HttpStatus.ok,
+            message: 'Login Successfully',
+            token: token,
+            body: loggedinUser.first,
+          );
   }
 }
