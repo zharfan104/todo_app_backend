96587854ad2b506c27f739eef932328c152f7370
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-11 19:53:46 +0700

    Update UserRequest

---
+++ lib/features/users/presentations/user_request.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+import 'package:dart_frog/dart_frog.dart';
+import 'package:todo_app_backend/core/helpers/response_helper.dart';
+import 'package:todo_app_backend/features/users/data/models/user_model.dart';
+import 'package:todo_app_backend/features/users/domain/usecases/add_user.dart';
+import 'package:todo_app_backend/features/users/domain/usecases/get_all_users.dart';
+import 'package:todo_app_backend/features/users/domain/usecases/get_user.dart';
+import 'package:todo_app_backend/features/users/domain/usecases/update_user.dart';
+
+class UserRequest {
+  const UserRequest._();
+
+  static Future<Response> updateUser(RequestContext context, int userId) async {
+    return _updateUser(context, userId);
+  }
+
+  static Future<Response> _updateUser(RequestContext context, int userId) async {
+    final userJson = await context.request.json() as Map<String, dynamic>;
+
+    userJson['id'] = userId;
+
+    final user = UserModel.fromJson(userJson);
+    final updateUserUsescase = UpdateUser();
+
+    await updateUserUsescase(user);
+
+    return ResponseHelper.json(
+      status: HttpStatus.ok,
+      message: 'Successfully update user',
+      body: user,
+    );
+  }
+
+  static Future<Response> getUser(RequestContext context, int userId) async {
+    return _getUser(context, userId);
+  }
+
+  static Future<Response> _getUser(RequestContext context, int userId) async {
+    final getUserUsescase = GetUser();
+
+    final user = await getUserUsescase(userId);
+
+    return ResponseHelper.json(
+      status: HttpStatus.ok,
+      message: 'Successfully get user',
+      body: user,
+    );
+  }
+
+  static Future<UserModel> addUser(RequestContext context) async {
+    return _addUser(context);
+  }
+
+  static Future<UserModel> _addUser(RequestContext context) async {
+    final userJson = await context.request.json() as Map<String, dynamic>;
+    final user = UserModel.fromJson(userJson);
+    final addUserUsescase = AddUser();
+
+    await addUserUsescase(user);
+
+    return user;
+  }
+
+  static Future<Response> getAllUsers(RequestContext context) async {
+    return _getAllUsers(context);
+  }
+
+  static Future<Response> _getAllUsers(RequestContext context) async {
+    final getAllUsersUsescase = GetAllUsers();
+
+    final users = await getAllUsersUsescase();
+    return ResponseHelper.json(
+      status: HttpStatus.ok,
+      message: 'Successfully get all users',
+      body: users,
+    );
+  }
+}
