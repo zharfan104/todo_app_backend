db81b7d6ee082f63f589e2bacf069b27dd1e3c03
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-19 00:49:03 +0700

    Add getProdConnection

--- lib/core/config/database_config.dart
+++ lib/core/config/database_config.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:injectable/injectable.dart';
 import 'package:postgres/postgres.dart';
 
@@ -5,16 +7,27 @@ import 'package:postgres/postgres.dart';
 abstract class RegisterModule {
   @preResolve
   Future<PostgreSQLConnection> database() async {
-    final database = PostgreSQLConnection(
-      'localhost',
-      5432,
-      'todoapp',
-      username: 'no_one',
-      password: 'no_one',
-    );
+    final database = getProdConnection();
 
     await database.open();
 
     return database;
+  }
+
+  PostgreSQLConnection getProdConnection() {
+    final connectionName = Platform.environment['CLOUD_SQL_CONNECTION_NAME']!;
+    final databaseName = Platform.environment['DB_NAME']!;
+    final user = Platform.environment['DB_USER']!;
+    final password = Platform.environment['DB_PASS']!;
+    final socketPath = '/cloudsql/$connectionName/.s.PGSQL.5432';
+
+    return PostgreSQLConnection(
+      socketPath,
+      5432,
+      databaseName,
+      username: user,
+      password: password,
+      isUnixSocket: true,
+    );
   }
 }
