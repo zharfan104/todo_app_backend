c789ef30987297518135551cb7e501c25fa40a02
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-12-20 07:04:21 +0700

    Add test case for PUT /task/[id]

--- test/routes/index_test.dart
+++ test/routes/index_test.dart
@@ -4,20 +4,21 @@ import 'package:dart_frog/dart_frog.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-import '../../routes/task/index.dart' as route;
+import '../../routes/task/index.dart' as task_with_index_route;
 
 class _MockRequestContext extends Mock implements RequestContext {}
 
+final _mockUri = Uri.parse('https://this-is-a-mock.url/');
+
 void main() {
-  group('GET /', () {
-    test('responds with a 200 and "Welcome to Dart Frog!".', () async {
+  group('PUT /task/[id]', () {
+    test('responds with a 405 (method not allowed)".', () async {
       final context = _MockRequestContext();
-      final response = await route.onRequest(context);
-      expect(response.statusCode, equals(HttpStatus.ok));
-      expect(
-        response.body(),
-        completion(equals('Welcome to Dart Frog!')),
-      );
+
+      when(() => context.request).thenReturn(Request.put(_mockUri));
+
+      final response = await task_with_index_route.onRequest(context);
+      expect(response.statusCode, equals(HttpStatus.methodNotAllowed));
     });
   });
 }
